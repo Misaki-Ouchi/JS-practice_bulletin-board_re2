5858ed1eb9c1c2c54c91981a465f1fc45c1c1856
ouchi <misaki.ouchi17@gmail.com>
2023-10-05 18:33:17 +0900

    ページネーション作成途中（配列分割まで）

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import WriteNewTitle from "./parts/links&btns/WriteNewTitle";
 import ThreadTitles from "./parts/threads/ThreadTitles";
 import ThreadsAreas from "./parts/threads/ThreadsAreas";
@@ -8,12 +8,63 @@ import { DataList } from "./Router";
 const MainPage = () => {
   const dataList = useContext(DataList);
   const titles = useContext(Titles);
+  
+  const [page, setPage] = useState(0);
+  let pageNum = [];
+  const handlePage = (e) => {
+    let type = e.target.value;
+    console.log(type);
+    if (type === "before") {
+      setPage(page - 1);
+    } else if (type === "next") {
+      setPage(page + 1);
+    } else {
+      type = Number(type);
+      setPage(type - 1);
+    }
+  };
+  const splitArray = (array, cutNumber) => {
+    const newArr = [];
+    console.log(dataList)
+    let j = 1;
+    for (let i = 0; i < array.length; i += cutNumber) {
+      newArr.push(array.slice(i, i + cutNumber));
+      pageNum.push(j);
+      j++;
+    }
+    return newArr;
+  };
+  let newList = splitArray(dataList, 5);
+  console.log(newList)
 
   return (
     <>
-        <WriteNewTitle />
-        <ThreadTitles titles={titles}/>
-        <ThreadsAreas count="4" dataList={dataList} />
+      <WriteNewTitle />
+      <ThreadTitles titles={titles} />
+      <ThreadsAreas count="4" dataList={dataList} />
+
+        {/* {newList[page].map((list, index) => {
+          return (
+            <ThreadsAreas key={index} count="4" dataList={list} />
+          );
+        })} */}
+      {page > 0 && (
+        <button onClick={handlePage} value="before">
+          前へ
+        </button>
+      )}
+      {pageNum.map((num, index) => {
+        return (
+          <button key={index} onClick={handlePage} value={num}>
+            {num}
+          </button>
+        );
+      })}
+      {page < pageNum.length - 1 && (
+        <button onClick={handlePage} value="next">
+          次へ
+        </button>
+      )} 
     </>
   );
 };
--- client/src/parts/threads/ThreadsAreas.js
+++ client/src/parts/threads/ThreadsAreas.js
@@ -2,9 +2,11 @@ import AThreadsArea from "./AThreadsArea";
 
 const ThreadsAreas = (props) => {
   const dataList = props.dataList;
+  console.log(dataList)
   return (
-    <div className="thread">
-      <div className="threadWrap">
+    <>
+      <div className="thread">
+        <div className="threadWrap">
           {dataList.map((value, index) => {
             return (
               <div key={index} className="threadsArea">
@@ -19,8 +21,42 @@ const ThreadsAreas = (props) => {
               </div>
             );
           })}
+        </div>
       </div>
-    </div>
+
+      {/* 
+        {newList[page].map((value, index) => {
+          return (
+            <div key={index} className="threadsArea">
+              <AThreadsArea
+                title={value.titleData.title}
+                comments={value.comments}
+                count={props.count}
+                comment_count={value.titleData.count}
+                title_id={value.titleData.id}
+                readAll="true"
+              />
+            </div>
+          );
+        })}
+      {page > 0 && (
+        <button onClick={handlePage} value="before">
+          前へ
+        </button>
+      )}
+      {pageNum.map((num, index) => {
+        return (
+          <button key={index} onClick={handlePage} value={num}>
+            {num}
+          </button>
+        );
+      })}
+      {page < pageNum.length - 1 && (
+        <button onClick={handlePage} value="next">
+          次へ
+        </button>
+      )} */}
+    </>
   );
 };
 
